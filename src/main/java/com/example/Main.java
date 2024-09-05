906c0d70afd97eead119c75f5d408fd27cfe3b09
dayson <dayson@gmail.com>
2024-09-05 09:18:56 -0500

    c3

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,10 +1,14 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int x = 32;
         int u = 53;
         System.out.println(x+u);
+
+        Scanner c2 = new Scanner( )
 
       }
 }
